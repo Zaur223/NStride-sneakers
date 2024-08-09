fc3439e92dee83f2f58a97aa4b629bd3d560d580
zaur <zhacizalov@bk.ru>
2024-08-09 19:31:09 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import {Fragment} from 'react';
+import './assets/fonts/fonts.scss';
+import Promo from './components/Promo';
+
+
+function App() {
+  return (
+    <Fragment>
+      <Promo />
+    </Fragment>
+  );
+}
+
+export default App;
