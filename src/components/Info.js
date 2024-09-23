8878662755479298b18ebfe64df9ef4f4f211b68
zaur <zhacizalov@bk.ru>
2024-09-23 20:32:09 +0300

    updated info.js

--- src/components/Info.js
+++ src/components/Info.js
@@ -2,21 +2,21 @@ import styles from '../styles/Info.module.scss';
 
 function Info() {
     return (
-        <div className={styles.info}>
+        <div id="info" className={styles.info}>
             <div className={styles.container}>
-                <div className={styles.text}>
-                    <h2 className={styles.maintext}>Lorem ipsum</h2>
-                    <ul>
-                        <li className={styles.subtext}>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</li>
-                        <li className={styles.subtext}>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </li>
-                        <li className={styles.subtext}>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. </li>
-                    </ul>
-                    <button className={styles.btn}><span>Show now</span></button>
-                </div>
-                <div className={styles.images}>
-                    <img src="images/nike1.png" alt=""/>
-                    <img src="images/nike2.png" alt=""/>
-                    <img src="images/nike3.png" alt=""/>
+                <div className={styles['info_wrapper']}>
+                    <div className={styles.text}>
+                        <h2 className={styles.maintext}>Lorem ipsum</h2>
+                        <ul>
+                            <li className={styles.subtext}>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</li>
+                            <li className={styles.subtext}>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </li>
+                            <li className={styles.subtext}>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. </li>
+                        </ul>
+                        <button className={styles.btn}><span>Show now</span></button>
+                    </div>
+                    <div className={styles.image}>
+                        <img src='images/nitro_site.png' alt='' />
+                    </div>
                 </div>
             </div>
         </div>
--- src/components/PromoContent.js
+++ src/components/PromoContent.js
@@ -5,9 +5,9 @@ function PromoContent() {
         <div className={styles.container}>
             <div className={styles['content_wrapper']}>
                 <div className={styles['content_wrapper-text']}>
-                    <h1 className={styles['content_wrapper-maintext']}>The 2019 Nike <br></br>Chinese Collection</h1>
-                    <h3 className={styles['content_wrapper-subtext']}>Two styles of graphics are used to represent this throughout the collection: The first is a traditional geometric quilt pattern that is influenced by all 12 Chinese Zodiac symbols; the second is a mosaic pattern of the 12 symbols.</h3>
-                    <button className={styles['content_wrapper-btn']}><span>Learn More</span></button>
+                    <h1 className={styles['content_wrapper-maintext']}>Welcome to <br/>our website Nitro</h1>
+                    <h3 className={styles['content_wrapper-subtext']}>Here you'll find the latest trends, trusted brands, and exclusive models that are perfect for both active adventures and everyday life. Comfort, style, and quality - all in one place. Visit our website to discover your perfect pair of sneakers that will highlight your unique style and provide comfort with every step!</h3>
+                    <a href='#info' className={styles['content_wrapper-btn']}><span>Learn More</span></a>
                 </div>
                 <div className={styles['content_wrapper-img']}>
                     <img className={styles['nike-back']} src='/images/sneaker_fon.png' alt=""/>
